8431e83624529ffc30e39649735a695ee29e395b
= <romagavr@hotmail.com>
2021-01-11 22:55:51 +0300

    inotify.c

--- tmp/inotify.c
+++ tmp/inotify.c
@@ -8,6 +8,9 @@
 #include<string.h>
 #include<stdint.h>
 
+#include<poll.h>
+#include<errno.h>
+
 #define BUF_LEN (10 * (sizeof(struct inotify_event) + NAME_MAX + 1))
 
 #define WATCH_PATH "/home/roman/Documents/clang/yandexDiskWebDav/build/inotTest/"
@@ -18,6 +21,26 @@ int inotifyFd = -1;
 static int addToWatch(const char *fpath, const struct stat *sb, int tflag, struct FTW *ftwbuf);
 static void displayInotifyEven(struct inotify_event *i);
 
+char **watchDir;
+int wdSize = 10;
+
+//struct for file modifying(for ".tmp editors")/moving files
+// may be send this?
+
+struct file_state {
+    char *name;
+
+    int create;
+    int modify;
+    int movedFrom;
+    int movedTo;
+
+    long exp_time;
+};
+struct file_state *files;
+int files_size;
+
+//Only directories added
 static int addToWatch(const char *fpath, const struct stat *sb, int tflag, struct FTW *ftwbuf){
     if (tflag == FTW_D) {
         int wd = inotify_add_watch(inotifyFd, fpath, IN_ALL_EVENTS);
@@ -25,6 +48,15 @@ static int addToWatch(const char *fpath, const struct stat *sb, int tflag, struc
             perror("inotify add");
             return 1;
         }
+        if (wd >= wdSize) {
+            char *n = realloc(watchDir, wd * 2);
+            if (n == 0) return 1;
+            watchDir = &n; 
+        }
+        watchDir[wd] = malloc(100);
+        strcpy(watchDir[wd], fpath);
+        //*(&(*watchDir) + wd)= malloc(100);
+        //strcpy(*(&(*watchDir) + wd), fpath);
         //printf("Directory %s (%s) was added to watchlist", fpath + ftwbuf->base, fpath);
     }
     return 0;
@@ -42,10 +74,18 @@ static void displayInotifyEven(struct inotify_event *i){
     if (i->mask & IN_MOVED_FROM && (flag = 1))    printf("IN_MOVED_FROM ");
     if (i->mask & IN_MOVED_TO && (flag = 1))      printf("IN_MOVED_TO ");
     if (i->mask & IN_UNMOUNT && (flag = 1))       printf("IN_UNMOUNT ");
+    //Create .tmp; write to .tmp; on save rename .tmp
+    // watch saved
+    if (flag) {
+        if (i->mask & IN_MOVED_TO) {
+            strcpy(files[files_size].name, watchDir[i->wd]);
+            printf("\n*****\n---- File %s/%s\n---- changed\n*****\n", watchDir[i->wd], i->name);
+        }
+    }
     if (flag){
-        printf("wd = %2d; ", i->wd);
+        printf("wd = %2d; path %s", i->wd, watchDir[i->wd]);
         if (i->len > 0)
-            printf("name = %s ", i->name);
+            printf(" name = %s ", i->name);
         if (i->cookie > 0)
             printf("cookie = %4d; ", i->cookie);
         printf("\n");
@@ -61,8 +101,15 @@ static void displayInotifyEven(struct inotify_event *i){
     //?if (i->mask & IN_Q_OVERFLOW)    printf("IN_Q_OVERFLOW ");
 };
 
+#define POLL_CAPASITY 1
+#define POLL_TIMEOUT -1
+
 int main() {
-    inotifyFd = inotify_init();
+    files = malloc(sizeof *files * 10);
+    watchDir = malloc(sizeof(char *) * wdSize);
+    if (watchDir == 0) return 1;
+
+    inotifyFd = inotify_init1(IN_NONBLOCK);
     if (inotifyFd == -1) {
         perror("inotify init");
         return 1;
@@ -72,24 +119,46 @@ int main() {
         perror("nftw error");
         return 1;
     }
-
+/*
+    struct pollfd *pfds = malloc(sizeof *pfds * POLL_CAPASITY);
+    if (pfds == 0) return 1;
+    pfds[0].fd = inotifyFd;
+    pfds[0].events = POLLIN;
+    int poll_count = 0; 
+    int size_in_ev = sizeof(struct inotify_event);
+*/
     ssize_t numRead;
     char buf[BUF_LEN];
     char *p = 0;
     struct inotify_event *event = 0;
     for (;;){
+       /* if ((poll_count = poll(pfds, POLL_CAPASITY, POLL_TIMEOUT)) <= 0) {
+			if (poll_count == 0) {
+				fprintf(stdout, "Message: poll timeout expired.\n");
+			} else {
+                printf("Erorr");
+				exit(EXIT_FAILURE);
+			}
+		}
+
+        if (((*pfds).revents & POLLIN)) {
+            for (p = buf; p < buf + numRead;){
+                event = (struct inotify_event *) p; 
+                displayInotifyEven(event);
+                p += size_in_ev + event->len;
+            }
+        }
+    }*/
         numRead = read(inotifyFd, buf, BUF_LEN);
         if (numRead == 0){
             perror("inotify read return 0");
-            continue;
-        }
-
-        if (numRead == -1){
-            perror("inotify failed");
             return 1;
         }
 
-        //printf("Read %ld bytes from inotify fd\n", (long) numRead);
+        if (numRead == -1 && errno != EAGAIN){
+            perror("inotify failed");
+            return 1;
+        }
 
         for (p = buf; p < buf + numRead;){
            event = (struct inotify_event *) p; 
